6f983f70ff4efd2329205902c0f4d07b63bdd1a2
Jahongir <jahongirnormuminov8@gmail.com>
2023-04-23 15:48:56 +0500

    new

--- data_import/get_data.py
+++ data_import/get_data.py
@@ -114,6 +114,7 @@ def invoices():
     while True:
         items = get_objects(url=url, session=session)
         results += [item for item in items['value']]
+        print()
         if '@odata.nextLink'in items:
             url = items['@odata.nextLink']
         else:
