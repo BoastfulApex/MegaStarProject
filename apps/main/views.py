72febdd1702e0c2534e8a31b13006b471e5cd458
BoastfulApex <jahongirnormuminov8@gmail.com>
2023-07-16 00:11:51 +0500

    test

--- apps/main/views.py
+++ apps/main/views.py
@@ -348,7 +348,7 @@ class DashboardListView(generics.ListAPIView):
 
 class CardView(viewlist.ListCreateAPIView):
     serializer_class = CardSerializer
-    permissions = [IsAuthenticatedCustom]
+    # permissions = [IsAuthenticatedCustom]
 
     def get_queryset(self):
         queryset = Card.objects.filter(user=self.request.user).all()
@@ -356,7 +356,7 @@ class CardView(viewlist.ListCreateAPIView):
 
     def create(self, request, *args, **kwargs):
         try:
-            card = Card.objects.filter(user=request.user, product=request.data['product']).first()
+            card = Card.objects.filter(user=request.data['user'], product=request.data['product']).first()
             if not card:
                 serializer = self.get_serializer(data=request.data)
                 serializer.is_valid(raise_exception=True)
